7d9c95d602868b789e8f891096526e6053baefcd
syler <hileo77@gmail.com>
2019-11-17 23:10:46 +0100

    Update BinaryParser.ts

--- src/BinaryParser.ts
+++ src/BinaryParser.ts
@@ -233,7 +233,7 @@ export class BinaryParser {
           );
         }
         // @ts-ignore
-        const inflate = Zlib.Zlib.Inflate(new Uint8Array(reader.getArrayBuffer(compressedLength))); // eslint-disable-line no-undef
+        const inflate = new Zlib.Zlib.Inflate(new Uint8Array(reader.getArrayBuffer(compressedLength))); // eslint-disable-line no-undef
         const reader2 = new BinaryReader(inflate.decompress().buffer);
 
         switch (type) {
